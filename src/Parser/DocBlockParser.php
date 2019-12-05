75111d5543386b1e2da714598904879c77edc5fb
Mathias Arlaud <mathias.arlaud@gmail.com>
2019-12-05 14:43:48 +0100

    Remove annotation repository

--- src/Parser/DocBlockParser.php
+++ src/Parser/DocBlockParser.php
@@ -6,25 +6,21 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\DocParser;
 use Mtarld\SymbokBundle\Factory\DocFactory;
 use Mtarld\SymbokBundle\Parser\DocBlockParser\Formatter;
-use Mtarld\SymbokBundle\Repository\AnnotationRepository;
 use phpDocumentor\Reflection\DocBlock;
 
 class DocBlockParser
 {
     private $formatter;
     private $docFactory;
-    private $annotationRepository;
 
     private $isContextReady = false;
 
     public function __construct(
         Formatter $formatter,
-        DocFactory $docFactory,
-        AnnotationRepository $annotationRepository
+        DocFactory $docFactory
     ) {
         $this->formatter = $formatter;
         $this->docFactory = $docFactory;
-        $this->annotationRepository = $annotationRepository;
     }
 
     public function parseAnnotations(DocBlock $docBlock): array
