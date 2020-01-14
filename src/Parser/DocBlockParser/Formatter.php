f8068d8817a081ce1273d907cde63680a6d0b416
Mathias Arlaud <mathias.arlaud@gmail.com>
2020-01-14 14:01:39 +0100

    Force Doctrine DocParser to process all annotations (#37)
    * Make Doctrine DocParser check ignoredAnnotations
    
    Ltrim \ for all Generic annotations
    
    * Update dependencies

--- src/Parser/DocBlockParser/Formatter.php
+++ src/Parser/DocBlockParser/Formatter.php
@@ -2,12 +2,10 @@
 
 namespace Mtarld\SymbokBundle\Parser\DocBlockParser;
 
-use Mtarld\SymbokBundle\Annotation\ToString;
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlock\Tags\BaseTag;
 use phpDocumentor\Reflection\DocBlock\Tags\Generic;
 use phpDocumentor\Reflection\FqsenResolver;
-use ReflectionClass;
 
 class Formatter
 {
@@ -23,23 +21,16 @@ class Formatter
 
     private function getResolvedTags(DocBlock $docBlock): array
     {
-        $symbokAnnotationNamespace = '\\'.(new ReflectionClass(ToString::class))->getNamespaceName();
-
-        return array_map(function (BaseTag $tag) use ($docBlock, $symbokAnnotationNamespace) {
+        return array_map(function (BaseTag $tag) use ($docBlock) {
             if (!$tag instanceof Generic) {
                 return $tag;
             }
 
             $resolvedTag = (new FqsenResolver())->resolve($tag->getName(), $docBlock->getContext());
-            $namespace = str_replace('\\'.$resolvedTag->getName(), '', (string) $resolvedTag);
 
-            if (0 === strcmp($symbokAnnotationNamespace, $namespace)) {
-                // ltrim \ in order to be sure that Doctrine DocParser will check ignoredAnnotations
-                // @see lib/Doctrine/Common/Annotations/DocParser.php:698
-                $resolvedTag = ltrim((string) $resolvedTag, '\\');
-            }
-
-            return new Generic((string) $resolvedTag, $tag->getDescription());
+            // ltrim \ in order to be sure that Doctrine DocParser will check ignoredAnnotations
+            // @see lib/Doctrine/Common/Annotations/DocParser.php:698
+            return new Generic(ltrim((string) $resolvedTag, '\\'), $tag->getDescription());
         }, $docBlock->getTags());
     }
 }
